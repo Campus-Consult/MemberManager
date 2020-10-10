d6f01ed8b15d797f3a90b6e77ae55da0a6941ba9
olohr <olohr@campus-consult.org>
2020-10-10 17:01:27 +0200

    Implement more position queries

---
+++ src/Application/Positions/Queries/GetPositionsWithAssignees/PositionAssignee.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MemberManager.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MemberManager.Application.Common.Mappings;
+using MemberManager.Domain.Entities;
+using System;
+
+namespace MemberManager.Application.Positions.Queries.GetPositionsWithAssignees
+{
+    public class PositionAssignee {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime BeginDateTime { get; set; }
+    }
+}

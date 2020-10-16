3e39a00f9bf7f7c3dab575a79ad389f61a710aca
Yorreck Wagner <ywagner@campus-consult.org>
2020-10-16 15:49:59 +0200

    Add MemberStatusDetailQuery

---
+++ src/Application/MemberStatus/Queries/GetMemberStatusDetail/MemberStatusDetailVm.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MemberManager.Application.Common.Interfaces;
+using System.Linq;
+
+namespace MemberManager.Application.MemberStatus.Queries.GetMemberStatusDetail
+{
+    public class MemberStatusDetailVm
+    {
+        private readonly IDateTime _dateTime;
+        public MemberStatusDetailVm(IDateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public bool IsActive { get; set; }
+        public bool CountAssignees { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.MemberStatus, MemberStatusDetailVm>()
+                .ForMember(d => d.CountAssignees, opt => opt.MapFrom(s => (int)s.PersonMemberStatus.Where(x => x.EndDateTime <= _dateTime.Now && x.BeginDateTime >= _dateTime.Now).Count()));
+        }
+    }
+}

9e6fecc7af6a23eb23a6eb8f8de9b5ce5248523d
Yorreck Wagner <ywagner@campus-consult.org>
2021-02-02 20:44:30 +0100

    Align positions query

--- src/Application/Positions/Queries/GetPositions/PositionLookupDto.cs
+++ src/Application/Positions/Queries/GetPositions/PositionLookupDto.cs
@@ -1,14 +1,30 @@
 ﻿using AutoMapper;
+using MemberManager.Application.Common.Interfaces;
 using MemberManager.Application.Common.Mappings;
 using MemberManager.Domain.Entities;
+using System.Linq;
 
 namespace MemberManager.Application.Positions.Queries.GetPositions
 {
     public class PositionLookupDto : IMapFrom<Position>
     {
+        private readonly IDateTime _dateTime;
+
+        public PositionLookupDto(IDateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
         public bool IsActive { get; set; }
+        public int CountAssignees { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Position, PositionLookupDto>()
+                .ForMember(d => d.CountAssignees, opt => opt.MapFrom(s => s.PersonPositions.Where(x => (x.EndDateTime == null || x.EndDateTime >= _dateTime.Now) && x.BeginDateTime <= _dateTime.Now).Count()));
+        }
     }
 }

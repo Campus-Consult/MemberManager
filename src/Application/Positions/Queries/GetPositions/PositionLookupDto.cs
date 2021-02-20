68eed413872804b7beaf0c66b006c124db3f5dce
Yorreck Wagner <ywagner@campus-consult.org>
2021-02-20 22:44:54 +0100

    Fix position queries and commands

--- src/Application/Positions/Queries/GetPositions/PositionLookupDto.cs
+++ src/Application/Positions/Queries/GetPositions/PositionLookupDto.cs
@@ -2,19 +2,13 @@
 using MemberManager.Application.Common.Interfaces;
 using MemberManager.Application.Common.Mappings;
 using MemberManager.Domain.Entities;
+using System;
 using System.Linq;
 
 namespace MemberManager.Application.Positions.Queries.GetPositions
 {
     public class PositionLookupDto : IMapFrom<Position>
     {
-        private readonly IDateTime _dateTime;
-
-        public PositionLookupDto(IDateTime dateTime)
-        {
-            _dateTime = dateTime;
-        }
-
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
@@ -24,7 +18,7 @@ namespace MemberManager.Application.Positions.Queries.GetPositions
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Position, PositionLookupDto>()
-                .ForMember(d => d.CountAssignees, opt => opt.MapFrom(s => s.PersonPositions.Where(x => (x.EndDateTime == null || x.EndDateTime >= _dateTime.Now) && x.BeginDateTime <= _dateTime.Now).Count()));
+                .ForMember(d => d.CountAssignees, opt => opt.MapFrom(s => s.PersonPositions.Where(x => (x.EndDateTime == null || x.EndDateTime >= DateTime.Now) && x.BeginDateTime <= DateTime.Now).Count()));
         }
     }
 }
---
+++ src/Application/Positions/Queries/GetPositionsHistory/PositionsHistoryVm.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MemberManager.Application.Common.Mappings;
+using MemberManager.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MemberManager.Application.Positions.Queries.GetPositionsHistory
+{
+    public class PositionsHistoryVm : IMapFrom<Domain.Entities.Position>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<AssigneeDto> Assignees { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.Position, PositionsHistoryVm>()
+                .ForMember(ms => ms.Assignees, opt => opt.MapFrom(s => s.PersonPositions.Select(a => new AssigneeDto() { PersonId = a.PersonId, Name = a.Person.FirstName + " " + a.Person.Surname, BeginDateTime = a.BeginDateTime, EndDateTime = a.EndDateTime }).OrderByDescending(a => a.BeginDateTime).ToList()));
+        }
+    }
+}

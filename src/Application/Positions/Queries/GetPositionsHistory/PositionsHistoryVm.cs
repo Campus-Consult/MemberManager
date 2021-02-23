16dc1dc428a2c12abd5632b87fa58a5351df9b6b
Yorreck Wagner <ywagner@campus-consult.org>
2021-02-23 12:42:13 +0100

    Replace qualifier with import

--- src/Application/Positions/Queries/GetPositionsHistory/PositionsHistoryVm.cs
+++ src/Application/Positions/Queries/GetPositionsHistory/PositionsHistoryVm.cs
@@ -1,13 +1,13 @@
 ﻿using AutoMapper;
 using MemberManager.Application.Common.Mappings;
 using MemberManager.Application.Common.Models;
-using System;
+using MemberManager.Domain.Entities;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace MemberManager.Application.Positions.Queries.GetPositionsHistory
 {
-    public class PositionsHistoryVm : IMapFrom<Domain.Entities.Position>
+    public class PositionsHistoryVm : IMapFrom<Position>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -15,7 +15,7 @@ namespace MemberManager.Application.Positions.Queries.GetPositionsHistory
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<Domain.Entities.Position, PositionsHistoryVm>()
+            profile.CreateMap<Position, PositionsHistoryVm>()
                 .ForMember(ms => ms.Assignees, opt => opt.MapFrom(s => s.PersonPositions.Select(a => new AssigneeDto() { PersonId = a.PersonId, Name = a.Person.FirstName + " " + a.Person.Surname, BeginDateTime = a.BeginDateTime, EndDateTime = a.EndDateTime }).OrderByDescending(a => a.BeginDateTime).ToList()));
         }
     }

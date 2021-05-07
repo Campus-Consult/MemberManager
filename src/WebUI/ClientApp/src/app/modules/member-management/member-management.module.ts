61439bbd6f1f025cf981788ba0d338ea1759cd72
Yorreck Wagner <yorreck.wagner@w-ays.de>
2021-05-07 20:45:30 +0200

    Add Material Datepicker to Member Form

--- src/WebUI/ClientApp/src/app/modules/member-management/member-management.module.ts
+++ src/WebUI/ClientApp/src/app/modules/member-management/member-management.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -69,6 +70,7 @@ import { MemberManagerRoutingModule } from './member-manager-routing.module';
     SharedModule,
     MatSortModule,
     MatButtonToggleModule,
+    MatDatepickerModule,
     FlexLayoutModule,
   ],
   entryComponents: [CreateMemberComponent, EditMemberDataComponent],

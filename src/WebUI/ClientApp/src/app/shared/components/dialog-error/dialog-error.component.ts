d9493855c55d39fa55f77781fd325c05a76d9c13
Alfer-Star <adrianalfermann@gmail.com>
2021-05-25 19:47:09 +0200

    tmp Component which displays error from server

---
+++ src/WebUI/ClientApp/src/app/shared/components/dialog-error/dialog-error.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-error',
+  templateUrl: './dialog-error.component.html',
+  styleUrls: ['./dialog-error.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DialogErrorComponent implements OnInit, OnChanges {
+
+  @Input() error;
+
+  errorMsg;
+  errorMsgList: string[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+    
+  ngOnChanges(changes: SimpleChanges): void {
+    this.handleError(this.error)
+  }
+
+  handleError(error) {
+    let parsedErrorObject = JSON.parse(error.response);
+
+    // TODO make error component
+    if (parsedErrorObject) {
+      this.errorMsg = parsedErrorObject.title + ":";
+      if (parsedErrorObject.errors) {
+        console.log('Werde ausgeführt');
+        this.errorMsgList = [];
+        for (const key in parsedErrorObject.errors) {
+          if (Object.prototype.hasOwnProperty.call(parsedErrorObject.errors, key)) {
+            this.errorMsgList.push( key + ': '+ parsedErrorObject.errors[key]);
+          }
+        }
+      }
+    } else {
+      console.error(error);
+    }
+  }
+
+}

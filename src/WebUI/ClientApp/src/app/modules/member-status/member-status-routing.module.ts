f28e1b5255c005bc8992097ad8e7f91aea7451b5
Adrian Alfermann <alferman@outlook.de>
2021-08-10 19:51:29 +0200

    issue80: Admin Guard added and Forbidden Page Routing

--- src/WebUI/ClientApp/src/app/modules/member-status/member-status-routing.module.ts
+++ src/WebUI/ClientApp/src/app/modules/member-status/member-status-routing.module.ts
@@ -1,10 +1,14 @@
+import { AdminGuard } from './../../guards/admin.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
+import { FORBIDDEN_ROUTE_STRING } from 'src/app/guards/admin.guard';
+import { PageForbiddenComponent } from 'src/app/shared/components/page-forbidden/page-forbidden.component';
 import { MemberStatusComponent } from './components/member-status/member-status.component';
 
 const routes: Routes = [
-  { path: 'member-status', component: MemberStatusComponent, canActivate: [AuthorizeGuard] }
+  { path: 'member-status', component: MemberStatusComponent, canActivate: [AuthorizeGuard, AdminGuard] },
+  { path: FORBIDDEN_ROUTE_STRING, component: PageForbiddenComponent }
 ];
 
 @NgModule({

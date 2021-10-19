84679c5f83d447fe0e96197f66ad9f632b8ff282
Adrian Alfermann <alferman@outlook.de>
2021-10-19 19:56:19 +0200

    Delete Dialog: generic title and content

--- src/WebUI/ClientApp/src/app/shared/components/delete-dialog/delete-dialog.component.ts
+++ src/WebUI/ClientApp/src/app/shared/components/delete-dialog/delete-dialog.component.ts
@@ -7,7 +7,7 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 })
 export class DeleteDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {name: string}) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {title:string, content: string}) { }
 
   ngOnInit(): void {
   }

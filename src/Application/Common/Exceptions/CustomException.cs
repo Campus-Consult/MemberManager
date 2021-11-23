994064bd5b189fd9e3311ae6e1b60d02b62c3de2
Yorreck Wagner <yorreck.wagner@w-ays.de>
2021-11-23 18:48:23 +0100

    Add Multi Error Exception

---
+++ src/Application/Common/Exceptions/CustomException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemberManager.Application.Common.Exceptions
+{
+    class MultiErrorException : Exception
+    {
+        public MultiErrorException(IList<string> errors)
+            : base("One or more errors have occurred.")
+        {
+            this.Errors = errors;
+        }
+
+
+        public IList<string> Errors { get; }
+    }
+}

a597bc63037015e3d963f16df99575a29806b290
Alfer-Star <alferman@outlook.de>
2022-04-26 19:52:37 +0200

    MemberSelfManaged self create if not exist

--- src/WebUI/ClientApp/src/app/modules/member-management/member-self-managed/member-self-managed.component.ts
+++ src/WebUI/ClientApp/src/app/modules/member-management/member-self-managed/member-self-managed.component.ts
@@ -1,25 +1,20 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PersonDetailVm } from 'src/app/membermanager-api';
+import { MemberFormComponent } from 'src/app/shared/components/member-form/member-form.component';
 import {
+  CreatePersonCommand,
+  ICreatePersonCommand,
   IUpdatePersonCommand,
   SelfManagementClient,
   UpdatePersonCommand,
 } from '../../../membermanager-api';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import {
-  CreatePersonCommand,
-  Gender,
-  ICreatePersonCommand,
-  PersonDetailVm,
-} from 'src/app/membermanager-api';
-import { MemberstatusCareerlevelService } from 'src/app/memberstatus-careerlevel.service';
-import { MemberFormComponent } from 'src/app/shared/components/member-form/member-form.component';
 
 @Component({
   selector: 'app-member-self-managed',
   templateUrl: './member-self-managed.component.html',
   styleUrls: ['./member-self-managed.component.scss'],
 })
-export class MemberSelfManagedComponent implements OnInit
-{
+export class MemberSelfManagedComponent implements OnInit {
   @ViewChild(MemberFormComponent) memberFormComp: MemberFormComponent;
 
   memberData: PersonDetailVm;
@@ -30,45 +25,59 @@ export class MemberSelfManagedComponent implements OnInit
   errorHintTitle = 'Person nicht erstellt:';
   invalidHints: Array<string>;
 
-  constructor(
-    protected selfManagementClient: SelfManagementClient
-  ) {
-  }
+  constructor(protected selfManagementClient: SelfManagementClient) {}
 
   ngOnInit(): void {
-    this.loadMemberData();  
+    this.loadMemberData();
   }
 
   loadMemberData() {
-
-    this.selfManagementClient
-      .getOverview()
-      .subscribe((value: PersonDetailVm) => {
-          this.memberData = value;
-      }, error => console.error(error));
+    this.selfManagementClient.getOverview().subscribe(
+      (value: PersonDetailVm) => {
+        this.memberData = value;
+      },
+      (error) => console.error(error)
+    );
   }
 
-  onSubmit() {
+  onCreateSubmit() {
     const command = this.convertCreateFormIntoCommand(
+      this.memberFormComp.personalForm.value
+    );
+    this.selfManagementClient.create(command).subscribe(
+      () => {
+        this.loadMemberData();
+        // TODO: Passe API an, dass die aktuelle Version vom Server kommt.
+        this.isEditing = false;
+      },
+      (error) => this.handleSubmitError(error)
+    );
+  }
+
+  onEditSubmit() {
+    const command = this.convertEditFormIntoCommand(
       this.memberFormComp.personalForm.value
     );
     this.selfManagementClient.update(command).subscribe(
       () => {
-        this.loadMemberData(); // TODO: Passe API an, dass die aktuelle Version vom Server kommt. 
+        this.loadMemberData();
+        // TODO: Passe API an, dass mit dem ersten Request die aktuelle Version vom Server kommt.
         this.isEditing = false;
       },
-      (error) => {
-        // TODO: show user error Message!
-        console.error(error);
-      }
+      (error) => this.handleSubmitError(error)
     );
+  }
+
+  handleSubmitError(error) {
+    console.error(error);
+    // TODO: show user error Message!
   }
 
   changeEditMode() {
     this.isEditing = !this.isEditing;
   }
 
-  private convertCreateFormIntoCommand(formResult: any): UpdatePersonCommand {
+  private convertEditFormIntoCommand(formResult: any): UpdatePersonCommand {
     const iCommand: IUpdatePersonCommand = {
       id: this.memberData.id,
       firstName: formResult.firstName,
@@ -84,5 +93,22 @@ export class MemberSelfManagedComponent implements OnInit
       adressCity: formResult.adressCity,
     };
     return new UpdatePersonCommand(iCommand);
+  }
+
+  private convertCreateFormIntoCommand(formResult: any): UpdatePersonCommand {
+    const iCommand: ICreatePersonCommand = {
+      firstName: formResult.firstName,
+      surname: formResult.lastName,
+      birthdate: formResult.birthdate,
+      gender: formResult.gender,
+      emailPrivate: formResult.emailPrivate,
+      emailAssociaton: formResult.emailAssociaton,
+      mobilePrivate: formResult.mobilePrivate,
+      adressStreet: formResult.adressStreet,
+      adressNo: formResult.adressNr,
+      adressZIP: formResult.adressZIP,
+      adressCity: formResult.adressCity,
+    };
+    return new CreatePersonCommand(iCommand);
   }
 }

1b0c2acaa8a21f90fae7d0d3cabc16b1e966ec12
Alfer-Star <alferman@outlook.de>
2022-04-26 18:38:50 +0200

    MemberSelfManaged display Flex on Home

--- src/WebUI/ClientApp/src/app/home/home.component.ts
+++ src/WebUI/ClientApp/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { AuthorizeService } from 'src/api-authorization/authorize.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styles:[".userinfo { display: flex; align-items: flex-start; justify-content: space-between;}",]
 })
 export class HomeComponent {
   public isAuthenticated: Observable<boolean>;

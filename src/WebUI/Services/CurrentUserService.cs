8c0d064224311068d82cb69632d67af26b7a476b
olohr <olohr@campus-consult.org>
2022-05-24 18:46:09 +0200

    remove Util class in favour of already existing current user service

--- src/WebUI/Services/CurrentUserService.cs
+++ src/WebUI/Services/CurrentUserService.cs
@@ -6,11 +6,17 @@ namespace MemberManager.WebUI.Services
 {
     public class CurrentUserService : ICurrentUserService
     {
+        private readonly string MAIL_CLAIM = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+        private readonly IHttpContextAccessor _httpContextAccessor;
         public CurrentUserService(IHttpContextAccessor httpContextAccessor)
         {
             UserId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public string UserId { get; }
+        public string GetEmailAssociate() {
+            return _httpContextAccessor.HttpContext?.User?.FindFirstValue(MAIL_CLAIM);
+        }
     }
 }

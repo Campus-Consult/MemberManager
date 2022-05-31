16ff004771d2b2808eebef1e9dd844d70e458407
Alfer-Star <alferman@outlook.de>
2022-05-31 19:16:23 +0200

    Event Table renamimg zusagen column to Teilnahmen

--- src/WebUI/ClientApp/src/app/modules/event-tracking/event-tracking-table/event-tracking-table.component.ts
+++ src/WebUI/ClientApp/src/app/modules/event-tracking/event-tracking-table/event-tracking-table.component.ts
@@ -1,4 +1,6 @@
+import { EventCodeDialogComponent } from './../event-code-dialog/event-code-dialog.component';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-event-tracking-table',
@@ -6,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-tracking-table.component.scss'],
 })
 export class EventTrackingTableComponent implements OnInit {
-  displayedColumns: string[] = ['eventname', 'tag', 'zusagen', 'qr-code'];
+  displayedColumns: string[] = ['eventname', 'tag', 'attendances', 'qrcode'];
 
   events: any[];
 
-  constructor() {}
+  constructor(protected dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.events = [];
+    for (let index = 0; index < 10; index++) {
+      this.events.push({
+        eventname: "Eventname" + index,
+        tag: 'VT',
+        attendances: index,
+        qrCode: "abcdefg"
+      });
+    }
+    for (let index = 0; index < 5; index++) {
+      this.events.push({
+        eventname: "MV" + index,
+        tag: 'MV',
+        attendances: index,
+        qrCode: "abcdefg"
+      });
+    }
+  }
 }
 
 export interface Event {}

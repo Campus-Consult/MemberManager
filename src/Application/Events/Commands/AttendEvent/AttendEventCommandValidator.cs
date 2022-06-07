5c9c81781a3401861222354694af355dcec83b2c
olohr <olohr@campus-consult.org>
2022-06-07 18:47:25 +0200

    check that event is active

--- src/Application/Events/Commands/AttendEvent/AttendEventCommandValidator.cs
+++ src/Application/Events/Commands/AttendEvent/AttendEventCommandValidator.cs
@@ -11,10 +11,12 @@ namespace MemberManager.Application.Events.Commands.AttendEvent
     public class CreateEventCommandValidator : AbstractValidator<AttendEventCommand>
     {
         private readonly IApplicationDbContext _context;
+        private readonly IDateTime _dateTime;
 
-        public CreateEventCommandValidator(IApplicationDbContext context)
+        public CreateEventCommandValidator(IApplicationDbContext context, IDateTime dateTime)
         {
             _context = context;
+            _dateTime = dateTime;
 
             RuleLevelCascadeMode = CascadeMode.Stop;
             RuleFor(v => v.EventId)
@@ -22,12 +24,19 @@ namespace MemberManager.Application.Events.Commands.AttendEvent
 
             RuleFor(v => v.EventSecretKey)
                 .NotEmpty()
-                .MustAsync(SecretKeyCheck).WithMessage("Event not found or bad key");
+                .MustAsync(SecretKeyCheck).WithMessage("Event not found or bad key")
+                .MustAsync(CheckEventActive).WithMessage("Event is not active!");
         }
 
         // for security we check both if the event exists and if the key matches, to give the same error in both cases
         public async Task<bool> SecretKeyCheck(AttendEventCommand command, string secretKey, CancellationToken cancellationToken) {
             return (await _context.Events.FindAsync(new object [] {command.EventId}, cancellationToken))?.SecretKey == secretKey;
+        }
+
+        public async Task<bool> CheckEventActive(AttendEventCommand command, string secretKey, CancellationToken cancellationToken) {
+            var evnt = await _context.Events.FindAsync(new object [] {command.EventId}, cancellationToken);
+            var now = _dateTime.Now;
+            return evnt.Start < now && evnt.End > now;
         }
     }
 }

6cfc2d46ec20b813cade27ec2b19d75f657188a2
Alfer-Star <alferman@outlook.de>
2022-07-12 20:25:29 +0200

    Some ground styling and errorvalidation

--- src/WebUI/ClientApp/src/app/modules/event-tracking/event-form/event-form.component.ts
+++ src/WebUI/ClientApp/src/app/modules/event-tracking/event-form/event-form.component.ts
@@ -1,33 +1,34 @@
-import {
-  PeopleClient,
-  ICreateEventCommand,
-} from './../../../membermanager-api';
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
-  FormControl,
+  ValidationErrors,
   Validators,
-  FormGroup,
 } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { PersonDetailVm, IPersonDetailVm } from 'src/app/membermanager-api';
-import { EventCodeDialogComponent } from '../event-code-dialog/event-code-dialog.component';
 import { Observable } from 'rxjs';
-import { startWith, map, pluck } from 'rxjs/operators';
+import { map, pluck, startWith } from 'rxjs/operators';
+import { IPersonDetailVm, PersonDetailVm } from 'src/app/membermanager-api';
+import { EventCodeDialogComponent } from '../event-code-dialog/event-code-dialog.component';
+import { ICreateEventCommand } from './../../../membermanager-api';
 
 @Component({
   selector: 'app-event-form',
   templateUrl: './event-form.component.html',
-  styleUrls: ['./event-form.component.scss']
+  styleUrls: ['./event-form.component.scss'],
 })
 export class EventFormComponent implements OnInit {
   @Input()
   suggOrganizer: IPersonDetailVm[] = [];
   @Input()
   suggTags: string[] = ['Peter', 'Max', 'Kevin'];
+  @Input()
+  startingTags: Set<string> = new Set(['VT']);
+  
+  tagsOnEvent = this.startingTags;
 
-  tagsOnEvent: Set<string> = new Set(['MV', 'VT']);
+  formError = { organizer: '' };
 
   // Auto Complete Observables, which handle valueChangeEvents
   filteredTagOptions: Observable<string[]>;
@@ -35,18 +36,24 @@ export class EventFormComponent implements OnInit {
 
   eventFormGroup = this.formBuilder.group({
     name: ['Event', [Validators.required]],
-    tags: [[this.tagsOnEvent], [Validators.required]],
+    tags: [
+      [this.tagsOnEvent],
+      {
+        validators: [Validators.required, (val) => this.isInternal(val)],
+        updateOn: 'blur',
+      },
+    ],
     tagInput: [''],
     organizer: ['', [Validators.required]],
-    eventDate: [Date.now()],
-    startTime: ['20:00'],
-    endTime: ['22:00'],
+    eventDate: [Date.now(), [Validators.required]],
+    startTime: ['20:00', [Validators.required]],
+    endTime: ['22:00', [Validators.required]],
   });
 
   constructor(
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<EventCodeDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   ngOnInit(): void {
@@ -56,9 +63,24 @@ export class EventFormComponent implements OnInit {
     }); */
 
     this.suggOrganizer = [
-      { firstName: 'Kevin', surname: 'Kvbe', emailAssociaton: 'aalf@cc.de' },
-      { firstName: 'Adrian', surname: 'rec', emailAssociaton: 'arec@cc.de' },
-      { firstName: 'chlor', surname: 'serbe', emailAssociaton: 'erbe@cc.de' },
+      {
+        id: 0,
+        firstName: 'Kevin',
+        surname: 'Kvbe',
+        emailAssociaton: 'aalf@cc.de',
+      },
+      {
+        id: 1,
+        firstName: 'Adrian',
+        surname: 'rec',
+        emailAssociaton: 'arec@cc.de',
+      },
+      {
+        id: 2,
+        firstName: 'chlor',
+        surname: 'serbe',
+        emailAssociaton: 'erbe@cc.de',
+      },
     ];
 
     // Filter Observable for tags and organizer
@@ -117,5 +139,20 @@ export class EventFormComponent implements OnInit {
     };
     // TODO: create output interface
     if (this.eventFormGroup.status) this.dialogRef.close(this.eventFormGroup);
+  }
+
+  isInternal(formControl: AbstractControl): ValidationErrors | null {
+    console.log('werde ausgeführt');
+
+    const isInternalOrganizer =
+      this.suggOrganizer.findIndex(
+        (value) => formControl.value.id === value.id
+      ) > -1;
+    this.formError.organizer = isInternalOrganizer
+      ? ''
+      : 'Person existiert nicht im Member Manager oder ist Teil von CC';
+    return isInternalOrganizer
+      ? { forbiddenName: { value: formControl.value } }
+      : null;
   }
 }

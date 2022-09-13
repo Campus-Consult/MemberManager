e06f2e43f6eaaf274fad31c7764517de81c7263c
Alfer-Star <alferman@outlook.de>
2022-09-13 20:12:31 +0200

    Fix doppelter Api Aufruf

--- src/WebUI/ClientApp/src/app/modules/event-tracking/event-tracking-table/event-tracking-table.component.ts
+++ src/WebUI/ClientApp/src/app/modules/event-tracking/event-tracking-table/event-tracking-table.component.ts
@@ -4,17 +4,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import {
-  EventDetailDto,
-  EventLookupDto,
-  UpdateEventCommand,
+  CreateEventCommand, EventLookupDto, UpdateEventCommand
 } from 'src/app/membermanager-api';
 import { DataTableComponent } from 'src/app/shared/components/data-table/data-table.component';
 import { EventCreateDialogComponent } from '../event-create-dialog/event-create-dialog.component';
 import { EventClient } from './../../../membermanager-api';
 import { EventCodeDialogComponent } from './../event-code-dialog/event-code-dialog.component';
 import {
-  EventFormComponent,
-  EventFormDialogData,
+  EventFormDialogData
 } from './../event-form/event-form.component';
 
 @Component({
@@ -58,23 +55,18 @@ export class EventTrackingTableComponent implements OnInit {
     // Open Form Dialog
     const dialogRef = this.dialog.open(EventCreateDialogComponent, {
       width: '450px',
-      // height: '700px',
       data: {
         suggestedTags: ['test'],
         startingTags: ['VT'],
+        submitAction: (createCommand: CreateEventCommand) =>
+          this.eventClient.create(createCommand),
       } as EventFormDialogData,
     });
 
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        const createCommand = result;
-        this.eventClient.create(createCommand).subscribe(
-          () => {
-            this.loadEvents();
-            this._snackBar.open(`${result.name} erstellt!`);
-          },
-          (err) => console.error(err)
-        );
+        this.loadEvents();
+        this._snackBar.open(`${result.name} erstellt!`);
       }
     });
   }
@@ -85,20 +77,6 @@ export class EventTrackingTableComponent implements OnInit {
       this.dataSource = new MatTableDataSource<EventLookupDto>(this.events);
       this.dataSource.sort = this.sort;
     });
-  }
-
-  onEdit(event: EventDetailDto) {
-    // Open Form Dialog
-    const dialogRef = this.dialog.open(EventFormComponent, {
-      width: '650px',
-      data: {
-        edit: event,
-        suggestedTags: ['test'],
-        startingTags: ['VT'],
-      } as EventFormDialogData,
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {});
   }
 
   async openEventDialog(event: EventLookupDto) {
@@ -119,8 +97,10 @@ export class EventTrackingTableComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(
       (result) => {
-        if (result)
+        if (result){
+          this.loadEvents();
           this._snackBar.open(`${result?.name} erfolgreich bearbeitet!`);
+        }
       },
       (err) => this._snackBar.open('Something went wrong.')
     );

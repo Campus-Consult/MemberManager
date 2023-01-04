f74e2ba41315adcd68783ffc4d1ce973386eff7f
Alexander Huebner <olex.07@hotmail.de>
2023-01-04 20:37:13 +0100

    Teilnehmer Button Interaktion

--- src/WebUI/ClientApp/src/app/modules/event-tracking/event-attendees-dialog/event-attendees-dialog.component.ts
+++ src/WebUI/ClientApp/src/app/modules/event-tracking/event-attendees-dialog/event-attendees-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { EventAnswerDto, EventClient } from 'src/app/membermanager-api';
+import { EventAnswerDto, EventClient, EventDetailDto, RemoveEventAnswerCommand } from 'src/app/membermanager-api';
 
 @Component({
   selector: 'app-event-attendees-dialog',
@@ -9,16 +9,28 @@ import { EventAnswerDto, EventClient } from 'src/app/membermanager-api';
 })
 export class EventAttendeesDialogComponent implements OnInit {
   public attendees:EventAnswerDto[];
-  displayedColumns: string[] = ['name', 'date'];
-
+  displayedColumns: string[] = ['name', 'date', 'delete'];
+  event:EventDetailDto;
+  
   constructor(private eventClient: EventClient, @Inject(MAT_DIALOG_DATA) public data: {id: number}) {
     this.eventClient.getSingle(data.id).subscribe((eventObject)=>{
-      this.attendees = eventObject.eventAnswers
+      this.attendees = eventObject.eventAnswers?? []
+      this.event = eventObject;
     })
     console.log(data.id)
    }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+  }
+
+  attendeeRemove(id:number){
+    const cmd = new RemoveEventAnswerCommand({id:id});
+    this.eventClient.removeEventAnswer(cmd).subscribe(()=>{
+      this.eventClient.getSingle(this.data.id).subscribe((eventObject)=>{
+        this.attendees = eventObject.eventAnswers
+        this.event = eventObject;
+      })
+    })
   }
 
 }

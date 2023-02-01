c0fdc0bfd502b6d13c10fda5d3eacdd12636109b
Alfer-Star <alferman@outlook.de>
2023-02-01 20:02:14 +0100

    Add EventAnswer Feature

--- src/WebUI/ClientApp/src/app/modules/event-tracking/event-attendees-dialog/event-attendees-dialog.component.ts
+++ src/WebUI/ClientApp/src/app/modules/event-tracking/event-attendees-dialog/event-attendees-dialog.component.ts
@@ -1,10 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {
   AddEventAnswerCommand,
   EventAnswerDto,
   EventClient,
   EventDetailDto,
+  IEventDetailDto,
+  IPersonLookupDto,
   RemoveEventAnswerCommand,
 } from 'src/app/membermanager-api';
 
@@ -16,20 +19,26 @@ import {
 export class EventAttendeesDialogComponent implements OnInit {
   public attendees: EventAnswerDto[];
   displayedColumns: string[] = ['name', 'date', 'delete'];
-  event: EventDetailDto;
+  event: IEventDetailDto = {name:'', start: '', end:''};
+
+  formGroup: FormGroup; 
 
   constructor(
     private eventClient: EventClient,
+    private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: { id: number }
   ) {
     this.eventClient.getSingle(data.id).subscribe((eventObject) => {
       this.attendees = eventObject.eventAnswers ?? [];
       this.event = eventObject;
     });
-    console.log(data.id);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      member: this.fb.control('', Validators.required)
+    })
+  }
 
   attendeeRemove(id: number) {
     const cmd = new RemoveEventAnswerCommand({ id: id });
@@ -42,6 +51,20 @@ export class EventAttendeesDialogComponent implements OnInit {
   }
 
   attendeeAdd() {
-    console.error('bitte implementier mich!');
+    const person = this.formGroup.get('member').value;
+    let id = undefined;
+    if(typeof person === 'object') {
+      id = (person as IPersonLookupDto).id;
+    } else {
+      console.warn('attendeeAdd: Could not extract personID');
+    }
+    const answerTime = new Date(this.event.start);
+    const cmd = new AddEventAnswerCommand({eventId: this.event.id, personId:id, answerTime: answerTime.toISOString()});
+    this.eventClient.addEventAnswer(cmd).subscribe(() => {
+      this.eventClient.getSingle(this.data.id).subscribe((eventObject) => {
+        this.attendees = eventObject.eventAnswers;
+        this.event = eventObject;
+      });
+    });
   }
 }
